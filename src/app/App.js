c3f891471ffef0d06697f6a351569781ee00842c
FrancoisMoanaMichael <91324853+FrancoisMoanaMichael@users.noreply.github.com>
2022-02-17 23:16:11 +0100

    base

---
+++ src/app/App.js
@@ -0,0 +1,121 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='game-name'>Game Name</div>
+      {/* player-card static   */}
+            <div className='player-card-container'>
+                  <div className='player-card'>
+                    <div className='player-card-name'>
+                      name
+                    </div>
+                    <div className='player-card-score'>
+                      score
+                    </div>
+                    <div className='player-card-total'>
+                      total
+                    </div>
+                  </div>
+                  {/* player-card static   */}
+                  <div className='player-card'>
+                    <div className='player-card-name'>
+                      name
+                    </div>
+                    <div className='player-card-score'>
+                      score
+                    </div>
+                    <div className='player-card-total'>
+                      total
+                    </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+        {/* player-card static   */}
+        <div className='player-card'>
+          <div className='player-card-name'>
+            name
+          </div>
+          <div className='player-card-score'>
+            score
+          </div>
+          <div className='player-card-total'>
+            total
+          </div>
+        </div>
+            </div>
+    </div>
+  );
+}
+
+export default App;
